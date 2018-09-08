7f6b74d9237ebe5d9dbecd937efaced73b91e955
pinakinc <chaubalpinakin@gmail.com>
2018-09-08 12:14:01 +0530

    Create Global_Constants.java

---
+++ Chapter_06/Global_Constants.java
@@ -0,0 +1,11 @@
+public interface Global_Constants {
+    public static final String CONFIGPATH = System.getProperty("user.dir")
+         + "\\src\\test\\testAutomation\\config\\config.properties";
+    public static final String ORPATH = System.getProperty("user.dir")
+         + "\\src\\test\\testAutomation\\Object_Repository\\OR.properties";
+ public static final String DRIVER = "DRIVER";
+ public static final String BROWSER = "BROWSER";
+ public static final String DRIVERPATH = "DRIVERPATH";
+ public static final String pageLoadStrategy_key = "pageLoadStrategy";
+ public static final String pageLoadStrategy_val = "eager";
+}
