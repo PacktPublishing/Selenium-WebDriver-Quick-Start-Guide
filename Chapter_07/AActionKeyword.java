076a083fdd0872658cc858193412c60faf373da3
pinakinc <chaubalpinakin@gmail.com>
2018-09-17 23:12:32 +0530

    Create AActionKeyword.java

---
+++ Chapter_07/AActionKeyword.java
@@ -0,0 +1,19 @@
+public class AActionKeyword implements IActionKeyword {
+        protected static WebDriver driver = null;
+        @Override
+        public void clickElement(String x) {
+        }
+        @Override
+        public WebDriver openBrowser() {
+                return null;
+        }
+        @Override
+        public void sendKeys(WebElement driver, String textFill) {
+        }
+        @Override
+        public void selectValue() {
+        }
+        public WebDriver navigate(WebDriver driver, String url) {
+                return null;
+        }
+}
