65bbf43585a0c4e73681d7a7e0276bbd7ba7d309
pinakinc <chaubalpinakin@gmail.com>
2018-09-17 23:13:22 +0530

    Create OpenBrowser.java

---
+++ Chapter_07/OpenBrowser.java
@@ -0,0 +1,6 @@
+public class OpenBrowser extends AActionKeyword {
+        public WebDriver openBrowser() {
+                WebDriver driver = new FirefoxDriver();
+                return driver;
+        }
+}
