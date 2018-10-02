bf8f5a8e40f30f7e76403743cb7ffdfdf7253f9d
pinakinc <chaubalpinakin@gmail.com>
2018-10-02 10:28:02 +0530

    Update J81Demo.java

--- Chapter_01/J81Demo.java
+++ Chapter_01/J81Demo.java
@@ -1,7 +1,16 @@
 package org.packt.selenium;
 
 public class J81Demo {
-	blockofCodeA = () -> {                                 
-	    System.out.println("Hello World");
+	public static void main(String[] args) {
+		LambdaTest blockofCodeA = () -> {
+			System.out.println("Hello World");
+		};
+		blockofCodeA.demo();
 	}
+}
+
+@FunctionalInterface
+interface LambdaTest {
+	void demo();
+
 }
