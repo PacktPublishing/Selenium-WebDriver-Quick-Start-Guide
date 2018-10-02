6abc73720e8bf18564b24278dbef79d97f7b1ae1
pinakinc <chaubalpinakin@gmail.com>
2018-10-02 10:53:13 +0530

    Update Demo.java

--- Chapter_01/Demo.java
+++ Chapter_01/Demo.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Demo {
-	List<String> fruits = Arrays.asList("Apples","Oranges","Bananas","Pears");
-	
-	fruits.stream().forEach(fruit -> System.out.println(fruit));
+
+	public static void main(String[] args) {
+		List<String> fruits = Arrays.asList("Apples", "Oranges", "Bananas",
+				"Pears");
+
+		fruits.stream().forEach(fruit -> System.out.println(fruit));
+	}
 }
