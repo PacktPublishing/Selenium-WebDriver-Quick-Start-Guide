f096154c17b54dca112a8ce9bdb71304fbe39583
pinakinc <chaubalpinakin@gmail.com>
2018-10-11 22:41:22 +0530

    Update JQueryLoadCheck.java

--- Chapter_05/JQueryLoadCheck.java
+++ Chapter_05/JQueryLoadCheck.java
@@ -4,12 +4,15 @@
  driver.navigate().to("http://localhost:8080/spinnerDemo.html");
  driver.findElement(By.xpath("//*[@id='changecontent']")).click();
  Boolean isJqueryCallDone = false;
- while (true) {
+ int counter = 0;
+ while (counter < 10) {
      isJqueryCallDone = (Boolean) ((JavascriptExecutor) driver)
                      .executeScript("return jQuery.active==0");
                  System.out.println("JQuery call in loop: " + isJqueryCallDone);
      if (isJqueryCallDone == true) {
          break;
+     } else {
+        counter++;
      }
   }
  System.out.println("JQuery call done: " + isJqueryCallDone);
