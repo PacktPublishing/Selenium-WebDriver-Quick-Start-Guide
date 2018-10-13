f173150706336662be5f7e31224e6dac707e3bf2
pinakinc <chaubalpinakin@gmail.com>
2018-10-13 11:38:13 +0530

    Update HoverDemo.java

--- Chapter_06/HoverDemo.java
+++ Chapter_06/HoverDemo.java
@@ -1,17 +1,18 @@
 public class HoverDemo {
     public static void main(String[] args) {
-         System.setProperty("webdriver.chrome.driver",
-             "C:\\SeleniumWD\\src\\main\\resources\\chromedriver.exe");
-         WebDriver driver = new ChromeDriver();
-         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-         driver.navigate().to("https://jqueryui.com/tooltip/");
-         driver.manage().window().maximize();
-         Actions actions = new Actions(driver);
-         driver.switchTo().frame(0);
-         actions.moveToElement(driver.findElement(By.id("age"))).build()
-                                         .perform();
-         System.out.println("Attribute is: "
-                         + driver.findElement(By.id("age")).getAttribute(
-                                                     "aria-describedby"));
-   }
+        String userDirectory = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",
+             userDirectory+"\\src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.navigate().to("https://jqueryui.com/tooltip/");
+        driver.manage().window().maximize();
+        Actions actions = new Actions(driver);
+        driver.switchTo().frame(0);
+        actions.moveToElement(driver.findElement(By.id("age"))).build()
+             .perform();
+        System.out.println("Attribute is: "
+             + driver.findElement(By.id("age")).getAttribute(
+                 "aria-describedby"));
+ }
 }
