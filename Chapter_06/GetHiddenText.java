1906638f36dd1f4f108e25a18609243a77d0bb32
pinakinc <chaubalpinakin@gmail.com>
2018-10-13 11:36:46 +0530

    Update GetHiddenText.java

--- Chapter_06/GetHiddenText.java
+++ Chapter_06/GetHiddenText.java
@@ -1,16 +1,16 @@
 public class GetHiddenText {
     public static void main(String[] args) {
-         System.setProperty("webdriver.chrome.driver",
-                 "C:\\SeleniumWD\\src\\main\\resources\\chromedriver.exe");
-         WebDriver driver = new ChromeDriver();
-         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-         driver.navigate().to(
-                 "C:\\Users\\Bhagyashree\\Documents\\DoubleClick.html");
-         driver.manage().window().maximize();
-         JavascriptExecutor js = (JavascriptExecutor) driver;
-         String txtPara = driver.findElement(By.id("txtPara"))
-                                             .getAttribute("id");
-         js.executeScript("document.getElementById('" + txtPara
-                                         + "').style.display='block';");
+        String userDirectory = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",
+             userDirectory+"\\src\\main\\resources\\chromedriver.exe");
+        RemoteWebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.navigate().to(
+             "C:\\Users\\Bhagyashree\\Documents\\DoubleClick.html");
+        driver.manage().window().maximize();
+        String txtPara = driver.findElement(By.id("txtPara"))
+             .getAttribute("id");
+        driver.executeScript("document.getElementById('" + txtPara
+             + "').style.display='block';");
  }
 }
