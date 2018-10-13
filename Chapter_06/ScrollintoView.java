8d2c1e8f781b9f744b9602b3b4492d7387ba9e4e
pinakinc <chaubalpinakin@gmail.com>
2018-10-13 11:41:51 +0530

    Update ScrollintoView.java

--- Chapter_06/ScrollintoView.java
+++ Chapter_06/ScrollintoView.java
@@ -1,15 +1,16 @@
 public class ScrollintoView {
     public static void main(String[] args) {
-         System.setProperty("webdriver.chrome.driver",
-             "C:\\SeleniumWD\\src\\main\\resources\\chromedriver.exe");
-         WebDriver driver = new ChromeDriver();
-         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-         driver.navigate().to("https://jqueryui.com");
-         driver.manage().window().maximize();
-         JavascriptExecutor js = (JavascriptExecutor) driver;
-         WebElement txtEasing = driver.findElement(By
-                                 .xpath("//a[text()='Easing']"));
-         js.executeScript("arguments[0].scrollIntoView(true);", txtEasing);
-         txtEasing.click();
+        String userDirectory = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",
+             userDirectory+"\\src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.navigate().to("https://jqueryui.com");
+        driver.manage().window().maximize();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement txtEasing = driver.findElement(By
+             .xpath("//a[text()='Easing']"));
+        js.executeScript("arguments[0].scrollIntoView(true);", txtEasing);
+        txtEasing.click();
  }
 }
