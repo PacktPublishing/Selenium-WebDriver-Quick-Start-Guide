78003af091574cabed8231cb6808410d3424b8d3
pinakinc <chaubalpinakin@gmail.com>
2018-10-13 11:39:48 +0530

    Update JQueryDBLClick.java

--- Chapter_06/JQueryDBLClick.java
+++ Chapter_06/JQueryDBLClick.java
@@ -1,15 +1,15 @@
 public class JQueryDBLClick {
-    public static void main(String[] args) {
-         System.setProperty("webdriver.chrome.driver",
-             "C:\\SeleniumWD\\src\\main\\resources\\chromedriver.exe");
-         WebDriver driver = new ChromeDriver();
-         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-         driver.navigate().to(
-             "C:\\Users\\Bhagyashree\\Documents\\DoubleClick.html");
-         Actions actions = new Actions(driver);
-         actions.doubleClick(driver.findElement(By.tagName("h2"))).build()
-                                                     .perform();
-         System.out.println("Text in para: "
-                             + driver.findElement(By.tagName("p")).getText());
-    }
+ public static void main(String[] args) {
+ String userDirectory = System.getProperty("user.dir");
+ System.setProperty("webdriver.chrome.driver",
+ userDirectory+"\\src\\main\\resources\\chromedriver.exe");
+ WebDriver driver = new ChromeDriver();
+ driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+ driver.navigate().to(
+ "C:\\Users\\Bhagyashree\\Documents\\DoubleClick.html");
+ Actions actions = new Actions(driver);
+ actions.doubleClick(driver.findElement(By.tagName("h2"))).perform();
+ System.out.println("Text in para: "
+ + driver.findElement(By.tagName("p")).getText());
+ }
 }
