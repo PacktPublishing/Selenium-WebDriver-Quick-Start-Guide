0fcf9e4c9b746083cfce875451560136d427bdf0
pinakinc <chaubalpinakin@gmail.com>
2018-10-14 16:22:37 +0530

    Update AActionKeyword.java

--- Chapter_07/AActionKeyword.java
+++ Chapter_07/AActionKeyword.java
@@ -1,19 +1,26 @@
-public class AActionKeyword implements IActionKeyword {
-        protected static WebDriver driver = null;
-        @Override
-        public void clickElement(String x) {
-        }
-        @Override
-        public WebDriver openBrowser() {
-                return null;
-        }
-        @Override
-        public void sendKeys(WebElement driver, String textFill) {
-        }
-        @Override
-        public void selectValue() {
-        }
-        public WebDriver navigate(WebDriver driver, String url) {
-                return null;
-        }
+public abstract class AActionKeyword extends TestBase {
+
+	public AActionKeyword() throws IOException, FilloException {
+		super();
+	}
+
+	public void clickElement(String x) {
+	}
+
+	public WebDriver navigate(WebDriver driver2, String url) {
+		return null;
+	}
+
+	public WebDriver openBrowser(List<String> browsers)
+			throws MalformedURLException {
+		return null;
+	}
+
+	public void sendKeys(WebElement elem, String textFill) {
+
+	}
+
+	public void selectValue() {
+	}
+
 }
