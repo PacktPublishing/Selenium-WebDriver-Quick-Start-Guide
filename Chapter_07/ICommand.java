2eb2f8c63ccc00cc2984b46c440b57ce463833c7
pinakinc <chaubalpinakin@gmail.com>
2018-10-14 16:21:05 +0530

    Delete ICommand.java

--- Chapter_07/ICommand.java
+++
@@ -1,7 +0,0 @@
-public interface ICommand {
-        static WebDriver driver = null;
-        public void execute(String x);
-        public void execute(WebDriver driver, String x);
-        public void execute(WebElement element, String x);
-        public WebDriver execute();
-}
