8ff9b27806910f361a53ea6ca06899fac09826c1
pinakinc <chaubalpinakin@gmail.com>
2018-10-14 16:24:39 +0530

    Update ACommand.java

--- Chapter_07/ACommand.java
+++ Chapter_07/ACommand.java
@@ -1,15 +1,20 @@
-public class ACommand implements ICommand {
-        @Override
-        public void execute(String x) {
-        }
-        @Override
-        public WebDriver execute() {
-                return driver;
-        }
-        @Override
-        public void execute(WebDriver driver, String x) {
-        }
-        @Override
-        public void execute(WebElement element, String x) {
-        }
+public abstract class ACommand {
+
+	public void execute(String x) {
+	}
+
+	public void execute(WebDriver driver, String x) {
+	}
+
+	public void execute(WebElement element, String x) {
+	}
+
+	public WebDriver execute(List<String> x) throws MalformedURLException {
+		return null;
+	}
+
+	public WebDriver execute() {
+		return null;
+	}
+
 }
