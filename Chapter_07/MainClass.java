122a11048996ed5000ade99fc5cbbe47c6161440
pinakinc <chaubalpinakin@gmail.com>
2018-10-14 16:28:46 +0530

    Update MainClass.java

--- Chapter_07/MainClass.java
+++ Chapter_07/MainClass.java
@@ -1,9 +1,15 @@
 public class MainClass {
-        public static void main(String[] args) throws InterruptedException {
-                WebDriver driver = null;
-                IActionKeyword actKeyword = new OpenBrowser();
-                ICommand command = new NewBrowser(actKeyword);
-                BrowserInvoker invoker = new BrowserInvoker(command);
-                driver = invoker.open();
-        }
+	public static void main(String[] args) throws IOException, FilloException {
+		WebDriver driver = null;
+		List<String> browsers = new ArrayList<String>();
+		browsers.add("chrome");
+		System.setProperty("webdriver.chrome.driver",
+				"src/main/resources/chromedriver.exe");
+		AActionKeyword actKeyword = new OpenBrowser();
+		ACommand command = new NewBrowser(actKeyword);
+		BrowserInvoker invoker = new BrowserInvoker(command);
+		driver = invoker.open(browsers);
+
+	}
+
 }
