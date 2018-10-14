57f895fd80a86ae87949fc1eb5886ca91787705c
pinakinc <chaubalpinakin@gmail.com>
2018-10-14 16:20:50 +0530

    Delete IActionKeyword.java

--- Chapter_07/IActionKeyword.java
+++
@@ -1,8 +0,0 @@
-public interface IActionKeyword {
-        WebDriver driver = null;
-        public void clickElement(String x);
-        public WebDriver navigate(WebDriver driver2, String url);
-        public WebDriver openBrowser();
-        public void sendKeys(WebElement elem, String textFill);
-        public void selectValue();
-}
