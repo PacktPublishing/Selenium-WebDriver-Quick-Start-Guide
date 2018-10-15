995d27576b150d959914d88cbc0cd6dac9d58fd4
pinakinc <chaubalpinakin@gmail.com>
2018-10-15 23:01:52 +0530

    Update HashMapExtract.java

--- Chapter_03/HashMapExtract.java
+++ Chapter_03/HashMapExtract.java
@@ -1,30 +1,25 @@
- Fillo fillo = new Fillo();
- Map<String, String> testcaseData = new HashMap<String, String>();
- Connection conn = null;
- try {
-         conn = fillo
+ public static void main(String[] args) {
+    Fillo fillo = new Fillo();
+    Map<String, String> testcaseData = new HashMap<String, String>();
+    Connection conn = null;
+    Recordset rcrdset = null;
+    try {
+      conn = fillo
           .getConnection("C:\\SeleniumWD\\src\\main\\resources\\Framework.xlsx");
-     } catch (FilloException e) {
-           e.printStackTrace();
-       }
- String query = "Select * from TestCases where TestCaseID in (select TestCaseID from TestConfig where Execute_Flag='Y')";
- Recordset rcrdset = null;
- try {
-         rcrdset = conn.executeQuery(query);
-     } catch (FilloException e) {
-         e.printStackTrace();
-       }
- try {
-         while (rcrdset.next()) {
-             testcaseData.put("TestCaseID", rcrdset.getField("TestCaseID"));
-             testcaseData.put("Keyword", rcrdset.getField("Keyword"));
-             testcaseData.put("Object", rcrdset.getField("Object"));
-             testcaseData.put("Data", rcrdset.getField("Data"));
-          }
-         System.out.println(testcaseData);
-     } catch (FilloException e) {
-             e.printStackTrace();
-       }
- rcrdset.close();
- conn.close();
-
+      String query = "Select * from TestCases where TestCaseID in (select                     TestCaseID from TestConfig where Execute_Flag='Y')";
+      rcrdset = conn.executeQuery(query);
+      while (rcrdset.next()) {
+        testcaseData.put("TestCaseID", rcrdset.getField("TestCaseID"));
+        testcaseData.put("Keyword", rcrdset.getField("Keyword"));
+        testcaseData.put("Object", rcrdset.getField("Object"));
+        testcaseData.put("Data", rcrdset.getField("Data"));
+      }
+      System.out.println(testcaseData);
+    } catch (FilloException e) {
+      throw new FilloException("Error in query processing");
+    } finally {
+      rcrdset.close();
+      conn.close();
+    }
+  }
+}
