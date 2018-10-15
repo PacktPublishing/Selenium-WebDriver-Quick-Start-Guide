947f071055ab72e58ff292cb77c30d4083820532
pinakinc <chaubalpinakin@gmail.com>
2018-10-15 22:59:01 +0530

    Update ExtractPropertyValue.java

--- Chapter_03/ExtractPropertyValue.java
+++ Chapter_03/ExtractPropertyValue.java
@@ -1,11 +1,19 @@
- Properties prop = new Properties();
- InputStream input = null;
- try {
- input = new FileInputStream(
- "C:\\SeleniumWD\\src\\main\\resources\\Login.properties");
- prop.load(input);
- System.out.println("Username: " + prop.getProperty("USERNAME"));
- System.out.println("Password: " + prop.getProperty("PASSWORD"));
- } catch (Exception e) {
-e.printStackTrace();
- }
+public class ReadProperties {
+
+	public static void main(String[] args) throws IOException {
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			input = new FileInputStream(
+					"C:\\SeleniumWD\\src\\main\\resources\\Login.properties");
+			prop.load(input);
+			System.out.println("Username: " + prop.getProperty("USERNAME"));
+			System.out.println("Password: " + prop.getProperty("PASSWORD"));
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException();
+		} finally {
+			input.close();
+		}
+
+	}
+}
