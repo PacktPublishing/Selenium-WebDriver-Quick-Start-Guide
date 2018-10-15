07c545edb19707cc264df77bdf30551aa572b313
pinakinc <chaubalpinakin@gmail.com>
2018-10-15 23:00:24 +0530

    Update ExtractSteps1..java

--- Chapter_03/ExtractSteps1..java
+++ Chapter_03/ExtractSteps1..java
@@ -1,28 +1,28 @@
-Fillo fillo = new Fillo();
- Connection conn = null;
- try {
-     conn =   
-     fillo.getConnection("C:\\SeleniumWD\\src\\main\\resources\\Framework.xlsx");
-     } catch (FilloException e) {
-         e.printStackTrace();
-       }
- String query = "Select * from TestCases where TestCaseID in (select TestCaseID   
-                 from TestConfig where Execute_Flag='Y')";
- Recordset rcrdset = null;
- try {
-         rcrdset = conn.executeQuery(query);
-     } catch (FilloException e) {
-         e.printStackTrace();
-     }
- try {
-         while (rcrdset.next()) {
-             System.out.println(rcrdset.getField("TestCaseID") + ":::"
-             + rcrdset.getField("Keyword") + ":::"
-             + rcrdset.getField("Object") + ":::"
-             + rcrdset.getField("Data"));
-         }
-     } catch (FilloException e) {
-         e.printStackTrace();
-       }
- rcrdset.close();
- conn.close();
+public class ExtractData2 {
+
+	public static void main(String[] args) throws FilloException {
+		Fillo fillo = new Fillo();
+		Connection conn = null;
+		Recordset rcrdset = null;
+		try {
+			conn = fillo
+					.getConnection("C:\\SeleniumWD\\src\\main\\resources\\Framework.xlsx");
+			String query = "Select * from TestCases where TestCaseID in (select TestCaseID from TestConfig where Execute_Flag='Y')";
+
+			rcrdset = conn.executeQuery(query);
+
+			while (rcrdset.next()) {
+				System.out.println(rcrdset.getField("TestCaseID") + ":::"
+						+ rcrdset.getField("Keyword") + ":::"
+						+ rcrdset.getField("Object") + ":::"
+						+ rcrdset.getField("Data"));
+			}
+		} catch (FilloException e) {
+			throw new FilloException("Error in query");
+		} finally {
+			rcrdset.close();
+			conn.close();
+		}
+	}
+
+}
