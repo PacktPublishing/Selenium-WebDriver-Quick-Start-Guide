2ca35ba84697542154d874cefb2e2e2136102ada
pinakinc <chaubalpinakin@gmail.com>
2018-10-18 06:27:22 +0530

    Update FilloTest.java

--- Chapter_02/FilloTest.java
+++ Chapter_02/FilloTest.java
@@ -1,29 +1,28 @@
 public class FilloTest {
 
-  public static void main(String[] args) {
-    Fillo fillo = new Fillo();
-    Connection conn = null;
-    try {
-      conn = fillo
-          .getConnection("C:\\SeleniumWD\\src\\main\\resources\\Framework.xlsx");
-    } catch (FilloException e) {
-      e.printStackTrace();
-    }
-    String query = "Select * from TestConfig where Execute_Flag='Y'";
-    Recordset rcrdset = null;
-    try {
-      rcrdset = conn.executeQuery(query);
-    } catch (FilloException e) {
-      e.printStackTrace();
-    }
-    try {
-      while (rcrdset.next()) {
-        System.out.println(rcrdset.getField("TestCaseID"));
-      }
-    } catch (FilloException e) {
-      e.printStackTrace();
-    }
-    rcrdset.close();
-    conn.close();
-  }
+	public static void main(String[] args) {
+		Fillo fillo = new Fillo();
+		Map<String, String> testcaseData = new HashMap<String, String>();
+		Connection conn = null;
+		Recordset rcrdset = null;
+		try {
+			conn = fillo
+					.getConnection("C:\\SeleniumWD\\src\\main\\resources\\Framework.xlsx");
+			String query = "Select * from TestCases where TestCaseID in (select TestCaseID from TestConfig where Execute_Flag='Y')";
+
+			rcrdset = conn.executeQuery(query);
+			while (rcrdset.next()) {
+				testcaseData.put("TestCaseID", rcrdset.getField("TestCaseID"));
+				testcaseData.put("Keyword", rcrdset.getField("Keyword"));
+				testcaseData.put("Object", rcrdset.getField("Object"));
+				testcaseData.put("Data", rcrdset.getField("Data"));
+			}
+			System.out.println(testcaseData);
+		} catch (FilloException e) {
+			e.printStackTrace();
+		} finally {
+			rcrdset.close();
+			conn.close();
+		}
+	}
 }
